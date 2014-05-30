9efc53cc914dd9681bbc6ad734644666c832b661
MoniaZam <monia_zam@wp.pl>
2014-05-31 00:52:33 +0200

    usuwanie wydarzenia

--- runApps/app/scripts/app.js
+++ runApps/app/scripts/app.js
@@ -13,6 +13,10 @@ angular
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+	  .when('/wydarzenia/usun/:id', {
+        templateUrl: 'views/events.html',
+        controller: 'EventsDeleteCtrl'
+      })
 	  .when('/wydarzenia', {
         templateUrl: 'views/events.html',
         controller: 'EventsCtrl'
@@ -30,7 +34,7 @@ angular
 		var db = openDatabase('events', '1.0', 'Events DB', 2 * 1024 * 1024);
 
 		db.transaction(function (tx) {
-		   //tx.executeSql('DROP TABLE Events');
+		   tx.executeSql('DROP TABLE Events');
 		   tx.executeSql('CREATE TABLE IF NOT EXISTS Events (id unique, name, description, timing)');
 		   tx.executeSql('INSERT INTO Events (id, name, description, timing ) VALUES (1, "Areobik w parku", "Areobik w parku Jordana", "Codziennie 19:00")');
 		   tx.executeSql('INSERT INTO Events (id, name, description, timing ) VALUES (2, "Rolkami po Krakowie", "Na rolkach wzdłóż Wisły", "Soboty 15:00")');	   
--- runApps/app/scripts/controllers/events.js
+++ runApps/app/scripts/controllers/events.js
@@ -33,10 +33,48 @@ angular.module('runappsApp')
     };
   
   })
+    .factory('DeleteEvents', function($q) {
   
-  .controller('EventsCtrl', function ($scope, Events) {
+    var removeEvent = function(id) {
+      var deferred = $q.defer();
+	    
+		var db = openDatabase('events', '1.0', 'Events DB', 2 * 1024 * 1024);
+		
+		db.transaction(function (tx) {
+		   tx.executeSql('DELETE FROM Events WHERE id='+id);
+		   tx.executeSql('SELECT * FROM Events', [], function (tx, results) {
+		   var len = results.rows.length, i;
+		   var response = new Array();
+		   for (i = 0; i < len; i++){
+				response.push({
+					'id' : results.rows.item(i).id,
+					'name' : results.rows.item(i).name,
+					'description' : results.rows.item(i).description,
+					'timing' : results.rows.item(i).timing 
+				});
+		   }
+		   deferred.resolve(response);
+		 }, null);
+		});
+	
+      return deferred.promise;
+    };
+    
+    return {
+      removeEvent: removeEvent
+    };
+  
+  })
+ .controller('EventsCtrl', function ($scope, Events) {
  
 		Events.getEvents().then(function(events) {
+			$scope.events = events;
+		});
+		
+})
+ .controller('EventsDeleteCtrl', function ($scope, $routeParams, DeleteEvents) {
+
+		DeleteEvents.removeEvent($routeParams.id).then(function(events) {
 			$scope.events = events;
 		});
 		
